d545fff12ff9ede3fd2dc614955527fdd2fc7231
weisintai <wstai1214@gmail.com>
2024-10-10 18:26:46 +0800

    Refactor component imports in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
-import DialogflowComponent from "@/components/dialogflow-form";
+import DialogflowComponent from "@/components/dialogflow-form/dialogflow-form";
+import { InstallPrompt } from "@/components/install-prompt";
+import { ModeToggle } from "@/components/mode-toggle";
 
 export default function Home() {
   return (
     <>
       <DialogflowComponent />
+      {/* <InstallPrompt />
+      <ModeToggle /> */}
     </>
   );
 }
